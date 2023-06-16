dbdc7d275cf3eb543f56f8523a78bec95f73c9b8
kriztiank <kriztiank@gmail.com>
2023-06-16 13:32:31 +0000

    restart

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,13 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>Hero to zero </div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>  
+      </div>
+    </div>
   )
 }
 export default Hero
